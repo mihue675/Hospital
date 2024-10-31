da39cbbff46daf4710fa6da4ba169029e24fd7e2
mihue675 <samirelcrack@hotmail.com>
2024-10-31 15:58:35 -0300

    Añadir logo

--- app/client/pages/auth/registrarse.php
+++ app/client/pages/auth/registrarse.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hospital - Registrarse</title>
     <link rel="stylesheet" href="registrarse.css">
+    <link rel="icon" href="../../images/logo.png" />
 </head>
 
 <?php
@@ -22,44 +23,44 @@ if (isset($_POST['btnRegistrarse']) && $_POST['txtEmail'] != "" && $_POST['txtCo
     <div class="divTodo">
 
         <div class="panel-derecho">
-        <h2>Registrarse</h2>
-        <form method="post" action="registrarse.php">
-            <div class="grupo-formulario">
-            <label>
-                Nombre:
-                <input type="text" name="txtNombre" id="txtNombre" value="" required />
-            </label>
-            <br />
-            <label>
-                Apellido:
-                <input type="text" name="txtApellido" id="txtApellido" value="" required />
-            </label>
-            <br />
-            </div>
-            <div class="grupo-formulario">
-            <label>
-                Email:
-                <input type=" email" name="txtEmail" id="txtEmail" value="" required />
-            </label>
-            <br />
-            <label>
-                Contraseña:
-                <input type="password" name="txtContraseña" id="txtContraseña" value="" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Debe contener al menos un número, una letra mayúscula y minúscula, y al menos 8 o más caracteres" required />
-            </label>
-            <br />
-            <button type="submit" name="btnRegistrarse" class="btn" id="btnRegistrarse">Registrarse</button>
-            </div>
+            <h2>Registrarse</h2>
+            <form method="post" action="registrarse.php">
+                <div class="grupo-formulario">
+                    <label>
+                        Nombre:
+                        <input type="text" name="txtNombre" id="txtNombre" value="" required />
+                    </label>
+                    <br />
+                    <label>
+                        Apellido:
+                        <input type="text" name="txtApellido" id="txtApellido" value="" required />
+                    </label>
+                    <br />
+                </div>
+                <div class="grupo-formulario">
+                    <label>
+                        Email:
+                        <input type=" email" name="txtEmail" id="txtEmail" value="" required />
+                    </label>
+                    <br />
+                    <label>
+                        Contraseña:
+                        <input type="password" name="txtContraseña" id="txtContraseña" value="" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Debe contener al menos un número, una letra mayúscula y minúscula, y al menos 8 o más caracteres" required />
+                    </label>
+                    <br />
+                    <button type="submit" name="btnRegistrarse" class="btn" id="btnRegistrarse">Registrarse</button>
+                </div>
             </form>
-            </div>
+        </div>
 
-            <div class="panel-izquierdo">
+        <div class="panel-izquierdo">
             <h2>¡Bienvenido!</h2>
             <p>Si ya tiene una cuenta, por favor inicie sesión.</p>
             <button onclick="window.location.href='iniciar-sesion.php'">Iniciar sesión</button>
-            </div>   
-    
+        </div>
+
     </div>
-    
+
 </body>
 
 </html>
