e02d59f5bc0ca2c33c53d2296255e2d2a1070b68
mihue675 <samirelcrack@hotmail.com>
2024-11-03 00:08:40 -0300

    componentes arreglados

---
+++ app/client/components/footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <html translate="no">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./footer.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <footer>
+        <p>&copy; 2024 Gestión de equipos médicos. Todos los derechos reservados.</p>
+        <p>Contacto: <a href="mailto:ucse@edu.com.ar">ucse@edu.com.ar</a></p>
+    </footer>
+
+
+</body>
+
+</html>
---
+++ app/client/components/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <html translate="no">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./header.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <header class='main-header'>
+        <nav class='nav-container'>
+            <ul class='nav-menu'>
+                <li class='has-submenu'>
+                    <a>Servicios</a>
+                    <ul class='submenu'>
+                        <li><a href='http://localhost/Hospital/app/client/pages/equipos/'>Equipos</a></li>
+                        <li><a href='http://localhost/Hospital/app/client/pages/mantenimientos/'>Mantenimientos</a></li>
+                        <li><a href='http://localhost/Hospital/app/client/pages/proveedores/'>Proveedores y contratos</a></li>
+                    </ul>
+                </li>
+                <li><a href=''>Acerca de</a></li>
+            </ul>
+            <div class='nav-right'>
+                <span class='icon-bell'>🔔</span>
+                <a href='http://localhost/Hospital/app/client/pages/auth/cerrar-sesion.php' class='logout'>Cerrar Sesión</a>
+            </div>
+        </nav>
+    </header>
+
+</body>
+
+</html>
